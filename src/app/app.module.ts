33e3df6613764b91a2b2d3930505abce7f4ef95d
Veeteq <witold.wojnarowicz@gmail.com>
2022-06-01 22:26:15 +0200

    CR-003 Create Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,    
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { NgModule } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
-  { path: '',   component: HomeComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '',        component: HomeComponent },
+  { path: 'login',   component: LoginComponent },
+  { path: '**',      pathMatch: 'full', redirectTo: '' }
 ];
 
 @NgModule({
