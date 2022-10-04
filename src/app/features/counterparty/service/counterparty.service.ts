725a4596f326b3c1248cafbf24d7ded739d65231
Veeteq <witold.wojnarowicz@gmail.com>
2022-10-04 22:57:06 +0200

    CR-021: Add New Counterparty functionality

--- src/app/features/counterparty/service/counterparty.service.ts
+++ src/app/features/counterparty/service/counterparty.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { Counterparty } from "../model/counterparty";
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,15 @@ export class CounterpartyService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getCounterpartiesByName(name: string): Observable<any[]> {    
+  getCounterpartiesByName(name: string): Observable<Counterparty[]> {    
     const findByNameUrl = `${this.apiUrl}/${name}`;
-    return this.httpClient.get<any[]>(findByNameUrl);
+    return this.httpClient.get<Counterparty[]>(findByNameUrl);
+  }
+
+  save(data: Counterparty): Observable<any> {
+    console.log("save: " + JSON.stringify(data));
+    const saveUrl = `${this.apiUrl}/save`;
+    return this.httpClient.post<any>(saveUrl, data);
   }
 
 }
